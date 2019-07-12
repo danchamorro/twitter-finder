685956db47b01a975056818faab6689ec46efc27
Dan Chamorro <dan@danchamorro.com>
2019-07-12 17:54:28 -0400

    Refactored UserItem to Functional Component

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,25 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class UserItem extends Component {
-  render() {
-    const { login, avatar_url, html_url } = this.props.user;
-
-    return (
-      <div className="card mt-3">
-        <img src={avatar_url} className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">{login}</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="#" className="btn btn-primary">
-            More
-          </a>
-        </div>
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className="card mt-3">
+      <img src={avatar_url} className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">{login}</h5>
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+        <a href={html_url} className="btn btn-primary">
+          More
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default UserItem;
