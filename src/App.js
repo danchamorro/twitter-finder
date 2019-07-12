91cc316e49ad54a66ad20f546fa5015b9b902286
Dan Chamorro <dan@danchamorro.com>
2019-07-12 17:02:32 -0400

    Add Users to APP.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
 
 export class App extends Component {
   render() {
     return (
-      <div>
+      <div className="App">
         <Navbar title="Twitter Showcase" icon="fab fa-twitter" />
+        <div className="container">
+          <Users />
+        </div>
       </div>
     );
   }
