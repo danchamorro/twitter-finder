c635490b88225b3cda96bb0193c1aee67fda76a6
Dan Chamorro <dan@danchamorro.com>
2019-07-18 14:14:16 -0400

    Added router and about page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import axios from "axios";
 import Tweets from "./components/tweets/Tweets";
 import Search from "./components/tweets/Search";
+import About from "./components/pages/About";
 
 export class App extends Component {
   state = {
@@ -22,13 +24,26 @@ export class App extends Component {
   render() {
     const { tweets } = this.state;
     return (
-      <div className="App">
-        <Navbar title="Twitter Showcase" icon="fab fa-twitter" />
-        <div className="container">
-          <Search searchTweets={this.searchTweets} />
-          <Tweets tweets={tweets} />
+      <Router>
+        <div className="App">
+          <Navbar title="Twitter Showcase" icon="fab fa-twitter" />
+          <div className="container">
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={props => (
+                  <Fragment>
+                    <Search searchTweets={this.searchTweets} />
+                    <Tweets tweets={tweets} />
+                  </Fragment>
+                )}
+              />
+              <Route exact path="/about" component={About} />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
